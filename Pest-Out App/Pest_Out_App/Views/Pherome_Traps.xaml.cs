18112c66a779403ea6f03ac3be9f0c2fe2549082
mzie20 <mzie20@gmail.com>
2017-07-21 03:28:37 +0200

    Add project files.

---
+++ Pest-Out App/Pest_Out_App/Views/Pherome_Traps.xaml.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Pest_Out_App.Views
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class Pherome_Traps : ContentPage
+    {
+        public Pherome_Traps()
+        {
+            InitializeComponent();
+        }
+
+        private async void Button_Clicked(object sender, EventArgs e)
+        {
+            await Navigation.PushAsync(new Pheromone_Shop());
+        }
+
+        private async void Button_Clicked_1(object sender, EventArgs e)
+        {
+            await Navigation.PushAsync(new TestPicture());
+        }
+
+    }
+}
